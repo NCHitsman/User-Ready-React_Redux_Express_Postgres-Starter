6f4a761f593a321da8e48b583fb9a7e0fd7d9235
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-06-08 21:28:09 -0700

    fin TS transition

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,36 +1,19 @@
 import { combineReducers } from "redux";
-import thunk from "redux-thunk";
 import sessionReducer from './session';
 import { configureStore } from '@reduxjs/toolkit'
 import {useDispatch} from 'react-redux'
+import {logger} from 'redux-logger'
 
 const rootReducer = combineReducers({
   session: sessionReducer,
 });
 
-
-
-let enhancer: any;
-
-if (process.env.NODE_ENV === "production") {
-  enhancer = [thunk];
-} else {
-  const logger = require("redux-logger").default;
-  enhancer = [thunk, logger];
-}
-
 export const store = configureStore({
   reducer: rootReducer,
-  enhancers: enhancer
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
-// const configureStore = (preloadedState?: any) => {
-//   return createStore(rootReducer, preloadedState, enhancer);
-// };
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
